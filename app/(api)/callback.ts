14c9d54f13893f14bfc0564a55405841bc3f2fdc
J4y J3ff <gaspergvj@gmail.com>
2024-05-08 21:01:11 +0300

    Trial once more

--- app/(api)/callback.ts
+++ app/(api)/callback.ts
@@ -1,23 +1,21 @@
 // pages/api/example.js
 
-export default async function handler() {
-  await fetch("https://ticket-system-orpin.vercel.app/callback", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+fetch("https://ticket-system-orpin.vercel.app/callback", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((res) => {
+    if (!res.ok) {
+      console.log("Problem");
+      return;
+    }
+    return res.json();
   })
-    .then((res) => {
-      if (!res.ok) {
-        console.log("Problem");
-        return;
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
