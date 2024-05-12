20538d7d323229996c3988624a6fd27677be257b
J4y J3ff <gaspergvj@gmail.com>
2024-05-12 17:27:43 +0300

    .

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -4,37 +4,8 @@ import { getUserInfo } from "@/lib/actions/ticket.action";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// pages/api/stkPushCallback.js
-
 export async function POST(req: any, res: any) {
   const data = await req.json();
   console.log(data);
-
-  const callbackData = data.Body.stkCallback.CallbackMetadata;
-
-  const result_code = callbackData.Body.stkCallback.ResultCode;
-  console.log(result_code);
-
-  const phoneObj = callbackData.Item.find(
-    (obj: any) => obj.Name === "PhoneNumber"
-  );
-  const phone: string = phoneObj.Value;
-  console.log(phone);
-
-  if (result_code !== 0) {
-    // If the result code is not 0, there was an error
-    const error_message = callbackData.Body.stkCallback.ResultDesc;
-    const response_data = {
-      ResultCode: result_code,
-      ResultDesc: error_message,
-    };
-    return NextResponse.json(response_data);
-  }
-
-  if (result_code === 0) {
-    const userInfo = await getUserInfo(phone);
-
-    console.log(userInfo);
-  }
-  return NextResponse.json({ message: "Successful Payment" });
+  return NextResponse.json({ message: "This is a POST Request" });
 }
