e26a62ef35e29c7b315b342d0d63a460af48ead6
J4y J3ff <gaspergvj@gmail.com>
2024-05-25 00:50:46 +0300

    add the thank you page.

---
+++ app/(root)/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Thank you for buying the 2024 Cultural show ticket. We have sent your
+      ticket to your email.
+    </div>
+  );
+};
+
+export default page;
--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -4,8 +4,10 @@ import { createUser } from "@/lib/actions/ticket.action";
 import { NextResponse } from "next/server";
 import qr from "qrcode";
 import nodemailer from "nodemailer";
+import { useRouter } from "next/navigation";
 
 export async function POST(req: any, res: any) {
+  const router = useRouter();
   const { searchParams } = new URL(req.url);
   const email = searchParams.get("email");
   const name = "J4y J3ff";
@@ -36,61 +38,68 @@ export async function POST(req: any, res: any) {
 
     const { email: userEmail, name: userName } = user;
     console.log(`User created: ${userName}, ${userEmail}`);
-  }
-  ////////////////////Email Payload////////////////////////////
-  const payload = {
-    phoneNumber: phone,
-    email: email,
-    name: name,
-  };
-  const jsonString = JSON.stringify(payload);
-  // const encoded_data = btoa(jsonString);
 
-  //////////////////////QR CODE/////////////////////////////////
-
-  qr.toDataURL(jsonString, { errorCorrectionLevel: "H" }, async (err, url) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    /////////////////////////Email -> NODEMAILER/////////////////////////
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: process.env.GOOGLE_APP_EMAIL,
-        pass: process.env.GOOGLE_APP_PASS,
-      },
-    });
-
-    let mailOptions = {
-      from: {
-        name: "Thought Be Things",
-        address: "gaspergvj@gmail.com",
-      },
-      to: email,
-      subject: "QR Code",
-      text: "Thank you for purchasing the ticket. The attached qr will be used for you verification at the entrance",
-      attachments: [
-        {
-          filename: "qrcode.png",
-          content: url.split("base64,")[1],
-          encoding: "base64",
-        },
-      ],
+    ////////////////////Email Payload////////////////////////////
+    const payload = {
+      phoneNumber: phone,
+      email: email,
+      name: name,
     };
+    const jsonString = JSON.stringify(payload);
+    // const encoded_data = btoa(jsonString);
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        return;
+    //////////////////////QR CODE/////////////////////////////////
+
+    qr.toDataURL(
+      jsonString,
+      { errorCorrectionLevel: "H" },
+      async (err, url) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        /////////////////////////Email -> NODEMAILER/////////////////////////
+
+        const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 587,
+          secure: false, // Use `true` for port 465, `false` for all other ports
+          auth: {
+            user: process.env.GOOGLE_APP_EMAIL,
+            pass: process.env.GOOGLE_APP_PASS,
+          },
+        });
+
+        let mailOptions = {
+          from: {
+            name: "Thought Be Things",
+            address: "gaspergvj@gmail.com",
+          },
+          to: email,
+          subject: "QR Code",
+          text: "Thank you for purchasing the ticket. The attached qr will be used for you verification at the entrance",
+          attachments: [
+            {
+              filename: "qrcode.png",
+              content: url.split("base64,")[1],
+              encoding: "base64",
+            },
+          ],
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            return;
+          }
+          console.log("Email sent: " + info.response);
+        });
       }
-      console.log("Email sent: " + info.response);
-    });
-  });
+    );
+
+    router.push("/thank-you");
+  }
 
   return NextResponse.json({ message: ".This is a POST Request." });
 }
