f7ef1cfa7cf9ae49bab267631b3243e6b0518f79
J4y J3ff <gaspergvj@gmail.com>
2024-05-25 01:09:28 +0300

    change thank-you page

--- app/(root)/thank-you/page.tsx
+++ app/(root)/thank-you/page.tsx
@@ -1,13 +1,15 @@
-"use client";
-import { useRouter } from "next/router";
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
 
-const ThankYouPage = () => {
-  const router = useRouter();
-  const { email, name } = router.query;
+const ThankYouPage = ({ searchParams }: PageProps) => {
+  const email = searchParams.email;
 
   return (
     <div>
-      <h1>Thank You, {name}!</h1>
+      <h1>Thank You, {email}!</h1>
       <p>
         We have received your payment. A confirmation email has been sent to{" "}
         {email}.
